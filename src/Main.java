1326395a5478494982deddc70d1942e79d8bd71f
muninnlabs <alexandreartedesigner@gmail.com>
2025-01-12 20:34:39 +0100

    Circle calculator implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        double radius = sc.nextDouble();
+
+        double c = Calculator.cirumference(radius);
+        double v = Calculator.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
+
+        sc.close();
+
     }
 }

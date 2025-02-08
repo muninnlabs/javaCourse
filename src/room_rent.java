2c111c2eaac4f71de203283062f0491727cb35ac
muninnlabs <alexandreartedesigner@gmail.com>
2025-02-08 21:40:34 +0100

    Exercicio fixacao finalizado pensionato

---
+++ src/room_rent.java
@@ -0,0 +1,42 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Vector;
+
+
+public class room_rent {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rent[] vect = new Rent[10];
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            System.out.println();
+            System.out.println("Rent #" + i + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+            vect[room] = new Rent(name, email);
+        }
+
+        System.out.println();
+        for(int i=0; i<10; i++) {
+            if (vect[i] != null) {
+                System.out.println(i + ": " + vect[i]);
+            }
+        }
+
+
+
+
+
+        sc.close();
+    }
+}

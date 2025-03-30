03185ed3025441f3cf319156218456af11e3e332
muninnlabs <alexandreartedesigner@gmail.com>
2025-03-30 17:58:17 +0200

    Exercicio Map exercice

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,49 @@
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-        System.out.println("hello world! ");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Map<String, Integer> votes = new LinkedHashMap<>();
+
+        String absolutePath = new File("").getAbsolutePath();
+        String path = absolutePath + "/resources/in.txt";
+
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            while (line != null) {
+
+                String[] fields = line.split(",");
+                String name = fields[0];
+                int count = Integer.parseInt(fields[1]);
+
+                if (votes.containsKey(name)) {
+                    votes.compute(name, (k, votesSoFar) -> count + votesSoFar);
+                } else {
+                    votes.put(name, count);
+                }
+                line = br.readLine();
+            }
+
+            for(String key : votes.keySet()) {
+                System.out.println(key + ": " + votes.get(key));
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+        sc.close();
     }
 }

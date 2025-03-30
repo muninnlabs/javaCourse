4f3344eab97705ec33d658982d398dfc518e8148
muninnlabs <alexandreartedesigner@gmail.com>
2025-03-30 16:52:16 +0200

    Exercicio Set

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,39 @@
-
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello world! ");
+        Set<String> set = new HashSet<>();
+        set.add("TV");
+        set.add("Notebook");
+        set.add("Tablet");
+        System.out.println(set.contains("Notebook"));
+        for (String p : set) {
+            System.out.println(p);
+        }
+
+        setFilters();
+
+    }
+
+    public static void setFilters(){
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
+
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println(d);
+
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
+
     }
 }

646e65d62911f7fec93f64a3f55fd439c632ebaa
muninnlabs <alexandreartedesigner@gmail.com>
2025-04-16 18:27:42 +0200

    Exercicio Lambda expressions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
+import entities.Product;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello world! ");
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+        double min = 100.0;
+
+        list.removeIf(p -> p.getPrice() >= min);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
     }
 }
---
+++ src/utils/ProductPredicate.java
@@ -0,0 +1,12 @@
+package utils;
+
+import entities.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+    @Override
+    public boolean test(Product product) {
+        return product.getPrice() >= 100;
+    }
+}
